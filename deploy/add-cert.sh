5fdcfe398c2327cd6c93a9e13b240681f9f57d1d
Волчок Григорий Владимирович <gvolchok@sportmasterlab.net>
2026-09-16 11:50:53 +0500

    Changes in adding ssl certificate

--- deploy/add-cert.sh
+++ deploy/add-cert.sh
@@ -4,12 +4,14 @@ add_cert() {
   cd "$INFRA_DIR"
 
   if [[ "$IS_PRODUCTION" == true ]] && [[ ! -f "$CERTIFICATE_PATH" ]]; then
+    # Создаем папки, если их нет
     mkdir -p certbot/www certbot/conf
 
     log "Stopping Nginx before the initial certificate issue"
     docker compose "${SERVER_COMPOSE_FILES[@]}" stop nginx || true
 
     log "Requesting a Let's Encrypt certificate for $DOMAIN"
+    # 1. Запускаем certbot как обычно (от root), чтобы у него были права внутри контейнера
     docker compose "${SERVER_COMPOSE_FILES[@]}" run --rm --service-ports certbot certonly \
       --standalone \
       --non-interactive \
@@ -18,7 +20,12 @@ add_cert() {
       --email "$LETSENCRYPT_EMAIL" \
       -d "$DOMAIN"
 
-      echo "############################## ADD_CERT [OK]"
+    log "Fixing permissions for certbot files"
+    # 2. Используем Docker для смены владельца на хосте. Пароль sudo НЕ потребуется!
+    docker compose "${SERVER_COMPOSE_FILES[@]}" run --rm --entrypoint chown certbot \
+      -R "$(id -u):$(id -g)" /etc/letsencrypt /var/lib/letsencrypt
+
+    echo "############################## ADD_CERT [OK]"
   else
     echo "############################## ADD_CERT [NO NEED]"
   fi
